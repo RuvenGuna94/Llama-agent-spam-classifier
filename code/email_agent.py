29d74c06a00b138af4249b8edf9d27cd6a9e2846
Ruven Guna <ruvenguna94@hotmail.com>
2025-02-03 21:44:02 +0800

    Update email_agent.py

--- code/email_agent.py
+++ code/email_agent.py
@@ -5,3 +5,9 @@ from crewai import Agent, Task, Crew, Process
 # Init model
 model = ollama(model = "llama3")
 
+# Create Agents for an email classifier
+# Specify email to classify
+email = "Inheritance from Nigerian Prince!! Click to collect USD$10M."
+
+
+# First agent receives email and second agent will write a response
