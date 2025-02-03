dd1aff8eeed8f8a1a5e64b8190a8c79f97bcab3a
Ruven Guna <ruvenguna94@hotmail.com>
2025-02-03 21:49:42 +0800

    Update email_agent.py
    Create first agent

--- code/email_agent.py
+++ code/email_agent.py
@@ -11,3 +11,13 @@ email = "Inheritance from Nigerian Prince!! Click to collect USD$10M."
 
 
 # First agent receives email and second agent will write a response
+# Create first agent
+classifier = Agent(
+    role = 'email classifier',
+    goal = "Accurately classify emails based on their importance, give every email on of these ratings: Important, Casual or Spam."
+    backstory = """Your only job is to classify emails. Do not hesitate to mark 
+    emails as casual if they are deemed not important. Your job is to help the user manage their inbox"""
+    verbose=True,
+    allow_delegation=False,
+    llm=model
+)
